f2e7fce5796f41f9483283fcca549c15568a23b7
Vako10f <146005001+Vako10f@users.noreply.github.com>
2024-02-12 18:30:16 +0400

    commit

--- projects/rockpaperscissors.py
+++ projects/rockpaperscissors.py
@@ -6,20 +6,19 @@ running = True
 while running:
 
     player = None
-    computer = random.choice(options)
-
-    while player not in options:
-        player = input("Enter a choice (rock, paper, scissors): ")
+    player2 = random.choice(options)
+    if player not in options:
+         player = input("Enter a choice (rock, paper, scissors): ")
     print(f"Player: {player}")
-    print(f"Computer: {computer}")
+    print(f"Computer: {player2}")
 
-    if player == computer:
-        print("It's a tie!")
-    elif player == "rock" and computer == "scissors":
+    if player == player2:
+        print("you both are winners")
+    elif player == "rock" and player2 == "scissors":
         print("You win!")
-    elif player == "paper" and computer == "rock":
+    elif player == "paper" and player2 == "rock":
         print("You win!")
-    elif player == "scissors" and computer == "paper":
+    elif player == "scissors" and player2 == "paper":
         print("You win!")
     else:
         print("You lose!")
