4ac60ca00b75e5953cc59275a99c0ae1ae2f58fc
Vako10f <146005001+Vako10f@users.noreply.github.com>
2024-02-13 17:07:36 +0400

    commit

---
+++ projects/kg to lbs.py
@@ -0,0 +1,7 @@
+option = ("kilogramm to lbs")
+running = True
+
+while running:
+    number = int(input("enter kilogramm"))
+    for multyplay in range(1) :
+        print(number,"*",2.204,"=",number*2.204)
