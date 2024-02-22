57559eb0305b0d41d3722c885186926bc03d09e3
Vako10f <146005001+Vako10f@users.noreply.github.com>
2024-02-22 20:33:24 +0400

    commit

---
+++ day6/homework/homework.py
@@ -0,0 +1,9 @@
+
+number1=int(input("please enter number1;"))
+number2=int(input("please enter number2;"))
+number3=int(input("please enter number3;"))
+
+print(number1+number2+number3)
+print(number1-number2-number3)
+print(number1*number2*number3)
+print(number1/number2/number3)
