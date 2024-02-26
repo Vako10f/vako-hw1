af2e0c6aaaa41d7e5cce87ea96f6583ce0a3f268
Vako10f <146005001+Vako10f@users.noreply.github.com>
2024-02-26 19:42:24 +0400

    commit

---
+++ day7/homework/homework.py
@@ -0,0 +1,12 @@
+print(int(4.8))
+print(int("7") + 4)
+
+print(float("2.7"))
+print(float(5.9))
+
+print(str(3) + "1")
+print(str(4.6) + "9")
+
+print(type(3.6))
+print(type(7))
+print(type("vako"))
---
+++ day8/classwork/classwork.py
@@ -0,0 +1,6 @@
+user_input=int(input("please enter number"))
+
+if user_input==5:
+    print("you won!")
+else:
+    print("you lose!")
