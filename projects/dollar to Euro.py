de5e5a36f180f6b81401f2d25d4e92f2cf00d103
Vako10f <146005001+Vako10f@users.noreply.github.com>
2024-03-11 00:18:06 +0400

    commit

--- projects/dollar to Euro.py
+++
@@ -1,8 +0,0 @@
-running = True
-
-while running:
-    number = int(input("enter dollar"))
-    for multyplay in range(1) :
-        print(number,"*",0.91,"=",number*0.91)
-
-        
--- projects/dollar to lari.py
+++
@@ -1,6 +0,0 @@
-running = True
-
-while running:
-    number = int(input("enter dollar"))
-    for multyplay in range(1) :
-        print(number,"*",2.66,"=",number*2.664)
