98ec9eb28ed02ad9bc27701e23fbd61d1a5300c4
Vako10f <146005001+Vako10f@users.noreply.github.com>
2024-03-11 17:32:47 +0400

    commit

--- day11/homework/homework.py
+++ day11/homework/homework.py
@@ -5,44 +5,53 @@ for i in range(1,51,5):
 for i in range (2,21,2):
     print(i)
 #3
+sum=1
 for i in range(5, 11):
-    num = i * i
-    print(f"{i} * {i} = {num}")
+    sum *= (i)
+print(f"some of numbers from5 to 10 is{sum}")
 #4
-print(input("enter number"))
-def fact( n):
-  if n>1:
-    return n*fact(n-1)
-  else:
-    return 1.
-n=int(input('enter the number'))
-print("Factorial of ",n,"is - ", fact(n))
+user_input=int(input("enter number: "))
+factorial=1
+for i in range (1,user_input+1):
+    factorial*=i
+print (f"factorial for {user_input} is {factorial} or {user_input}! is {factorial}")
+
+
 #5
-print(input("please enter number"))
-num=input
-if num/2==int:
-   print("it's even")
-elif num:
-   print("it's oven")
+user_input = int(input("Enter whole number: "))
+if user_input % 2 == 0:
+    user_input /= 2
+    print(int(user_input))
+else:
+    new_num = user_input * 3 + 1
+    print(new_num)
 #6
-for i in range(10, 1, -1):
-    print(i)
+num2=10
+
+while num2>=1:
+    print(num2)
+    num2-=1
+
+
 #7
 name=str(input("Please enter your name: "))
-if name =="quit":
-   print("you are in")
-elif name!="quit":
-   print("its incorrect")
-   print("try again")
+while name!="quit":
+    name=str(input("Please enter your name: "))
+
 #8
-for i in range(10,21,2):
-   print(i)
+num1=10
+while num1<20:
+    print(num1)
+    num1+=2
 #9
-num =int(input("Please enter positive number: "))
-if num >0:
-   print("you have written positive number.")
-elif num <0:
-    (print("you have written negative number."))
+num =int(input("Please enter number: "))
+while num<=0:
+    num =int(input("Please enter number: "))
+
+
 #10
-for i in range(1, 11):
-    print(i**2)
+num=1
+while num<=10:
+    new_num=num**2
+    print(new_num)
+    num+=1
