e78648b8682ce51ac3f361fceeed7b67eb13c9e7
Vako10f <146005001+Vako10f@users.noreply.github.com>
2024-03-29 18:07:47 +0400

    commit

---
+++ day16/classwork/classwork.py
@@ -0,0 +1,13 @@
+#1
+cars= {"mclaren","lamborgini","nissan","bmw","mercedes" }
+print (cars[0])
+print (cars[1])
+print (cars[2])
+#2
+name={"vako","natsvlishvili"}
+print(name[0],name[1])
+#3
+age=int(input("enter your age"))
+mail=input("enter your mail")
+user={age,mail}
+print(user[0],user[1])
---
+++ day16/homework/homework.py
@@ -0,0 +1,28 @@
+#1
+family_member= {"lui","mariana","nia","niko","ilia" }
+print (family_member[0])
+print (family_member[1])
+print (family_member[2])
+print (family_member[3])
+print (family_member[4])
+#2
+numbers={"1","2","3","4","5","6","7","8","9","10"}
+print(numbers[0])
+print(numbers[9])
+#3
+numbers2={ "10","11","12","13","14","15","16","17","18","19","20"}
+#არვიცი როგორ გავაკეთო ამის გარეშე
+if numbers2 % 2 == 0:
+    print("1")
+#4
+name=input("enter your name")
+surname=input("enter your surname")
+mail=input("enter your mail")
+age=input("enter your age")
+place=input("enter place where you live")
+list1={name,surname,mail,age,place}
+#5
+surname = "natsvlishvili"
+for i in range(len(surname)):
+    print(surname[i])
+
