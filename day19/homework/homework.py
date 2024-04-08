78c8e5fd262dd2e350bac98007d272099678e4d3
Vako10f <146005001+Vako10f@users.noreply.github.com>
2024-04-08 17:30:53 +0400

    commit

---
+++ day19/homework/homework.py
@@ -0,0 +1,14 @@
+names=["vako", "dato", "deme", "nika",""]
+input1=int(input("Please enter number from 0 to 4"))
+if input1 == 0:
+    print(names[0])
+elif input1 == 1:
+    print(names[1])
+elif input1 == 2:
+    print(names[2])
+elif input1 == 3:
+    print(names[3])
+elif input1 == 4:
+    print(names[4])
+else:
+    print("try again")
