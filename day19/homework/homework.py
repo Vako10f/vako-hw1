c25a8b1cbabe48036c6a0c1e90117a5d2f29b89c
Vako10f <146005001+Vako10f@users.noreply.github.com>
2024-04-08 17:33:35 +0400

    commit

--- day19/homework/homework.py
+++ day19/homework/homework.py
@@ -1,5 +1,5 @@
-names=["vako", "dato", "deme", "nika",""]
-input1=int(input("Please enter number from 0 to 4"))
+names=["vako", "dato", "deme", "nika","mari","gio"]
+input1=int(input("Please enter number from 0 to 5"))
 if input1 == 0:
     print(names[0])
 elif input1 == 1:
@@ -10,5 +10,7 @@ elif input1 == 3:
     print(names[3])
 elif input1 == 4:
     print(names[4])
+elif input1 == 5:
+    print(names[5])
 else:
     print("try again")
