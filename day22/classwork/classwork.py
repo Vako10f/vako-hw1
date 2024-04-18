457910db4166fec0704e93dc3481641c804ae377
Vako10f <146005001+Vako10f@users.noreply.github.com>
2024-04-18 17:04:17 +0400

    commit

---
+++ day22/classwork/classwork.py
@@ -0,0 +1,31 @@
+numbers=["vako","nika","dato","sandro","mate","cotne"]
+print(numbers[0:3])
+
+nums=[1,2,3,4,5,6]
+print(nums[4:6])
+
+list=[]
+start=0
+end=20
+step=2
+while start>end:
+    list.append(start)
+    start=start+step
+print(list)
+ 
+ 
+
+
+numbers = []
+
+start = 5
+end = 0
+step = -1
+
+while start > end:
+    numbers.append(start)
+    start = start + step
+
+print(numbers)
+
+
