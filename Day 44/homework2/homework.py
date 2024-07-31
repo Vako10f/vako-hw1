2fd8ea3cc0cb1a895cf71fd8dd7a2e0aeea69156
Vako10f <146005001+Vako10f@users.noreply.github.com>
2024-07-31 23:05:35 +0400

    commit

---
+++ Day 44/homework2/homework.py
@@ -0,0 +1,29 @@
+print("hello world")
+age=(input("enter your age"))
+#goa
+"""
+brw5wb
+btwbwb
+"""
+
+for i in range(10):
+    if 5<4:
+        break
+
+print("world")#print world
+for i in range(10):
+    if 5<4:
+        break
+"""
+range is 10 and it must break because 5 is not < than four
+
+"""
+print(hello world)#its not correct it neads ""
+
+name=("ilia")
+print(name+2)
+
+
+age=(input("enter your age"))
+
+
